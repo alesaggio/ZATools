7dddc1d563d53dc876b43736310c61e046f3ff95
alesaggio <saggio.alessia@gmail.com>
2018-08-20 10:56:28 +0200

    remove overFlow bin in rho plots

--- factories_ZA/launchFactory.py
+++ factories_ZA/launchFactory.py
@@ -142,28 +142,28 @@ MainPlots_ForMC = Configuration('generatePlots.py', suffix='_for_MCbkg', mode='p
             #"QCD"
         ], generation_args={
             'sample_type': 'MC',
-            'lljj_plots': ['basic', 'inEllipse', 'outOfEllipse', 'inOut'],
-            'llbb_plots': ['basic', 'inEllipse', 'outOfEllipse', 'inOut'],
+            'lljj_plots': ['basic', 'inOut'],
+            'llbb_plots': ['basic', 'inOut'],
             'syst': True,
-            'syst_split_jec': False,
-            'lljj_stages': ['no_cut', 'mll_and_met_cut', 'inverted_met_cut', 'met_cut_and_inverted_mll_cut', 'mll_cut_and_inverted_met_cut', 'mll_and_met_cut_and_mbb_cut'],
-            'llbb_stages': ['no_cut', 'mll_and_met_cut', 'inverted_met_cut', 'met_cut_and_inverted_mll_cut', 'mll_cut_and_inverted_met_cut', 'mll_and_met_cut_and_mbb_cut'],
+            'syst_split_jec': True,
+            'lljj_stages': ['no_cut', 'mll_and_met_cut'],
+            'llbb_stages': ['no_cut', 'mll_and_met_cut'],
         })
 MainPlots_ForData = Configuration('generatePlots.py', suffix='_for_data', mode='plots', samples=['Data'], generation_args={
             'sample_type': 'Data',
-            'lljj_plots': ['basic', 'inEllipse', 'outOfEllipse', 'inOut'],
-            'llbb_plots': ['basic', 'inEllipse', 'outOfEllipse', 'inOut'],
+            'lljj_plots': ['basic','inOut'],
+            'llbb_plots': ['basic','inOut'],
             'syst': True,
-            'syst_split_jec': False,
-            'lljj_stages': ['no_cut', 'mll_and_met_cut', 'inverted_met_cut', 'met_cut_and_inverted_mll_cut', 'mll_cut_and_inverted_met_cut', 'mll_and_met_cut_and_mbb_cut'],
-            'llbb_stages': ['no_cut', 'mll_and_met_cut', 'inverted_met_cut', 'met_cut_and_inverted_mll_cut', 'mll_cut_and_inverted_met_cut', 'mll_and_met_cut_and_mbb_cut'],
+            'syst_split_jec': True,
+            'lljj_stages': ['no_cut', 'mll_and_met_cut'],
+            'llbb_stages': ['no_cut', 'mll_and_met_cut'],
         })
 MainPlots_ForSignal = Configuration('generatePlots.py', suffix='_for_signal', mode='plots', samples=['Signal'], generation_args={
             'sample_type': 'Signal',
-            'llbb_plots': ['basic', 'inEllipse', 'outOfEllipse', 'inOut'],
+            'llbb_plots': ['basic','inOut'],
             'syst': True,
-            'syst_split_jec': False,
-            'llbb_stages': ['no_cut', 'mll_and_met_cut', 'inverted_met_cut', 'met_cut_and_inverted_mll_cut', 'mll_cut_and_inverted_met_cut', 'mll_and_met_cut_and_mbb_cut'],
+            'syst_split_jec': True,
+            'llbb_stages': ['no_cut', 'mll_and_met_cut'],
         })
 
 """
